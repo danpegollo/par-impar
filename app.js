94faae48345a0ff22b2766e8a57c577a8b0cbf2e
danpegollo <danielpegollo29@gmail.com>
2025-08-07 16:09:48 -0300

    operador ternario

--- app.js
+++ app.js
@@ -3,26 +3,22 @@ function verificar() {
   let resto = numero % 2;
   let parTexto = document.querySelector("#par");
   let imparTexto = document.querySelector("#impar");
-
+ 
   parTexto.classList.remove("verde", "vermelho");
   imparTexto.classList.remove("verde", "vermelho");
-
-  if (resto === 0) {
-    parTexto.classList.add("verde");
-    imparTexto.classList.add("vermelho");
-  } else {
-    parTexto.classList.add("vermelho");
-    imparTexto.classList.add("verde");
-  }
+ 
+  resto === 0
+    ? parTexto.classList.add("verde") || imparTexto.classList.add("vermelho")
+    : parTexto.classList.add("vermelho") || imparTexto.classList.add("verde");
 }
 function reiniciar() {
   document.querySelector("#n1").value = "";
   let parTexto = document.querySelector("#impar");
   let imparTexto = document.querySelector("#par");
-
+ 
   parTexto.classList.remove("verde", "vermelho");
   imparTexto.classList.remove("verde", "vermelho");
 }
-
+ 
 document.querySelector("#botao").addEventListener("click", verificar);
 document.querySelector("#botao_reiniciar").addEventListener("click", reiniciar);
